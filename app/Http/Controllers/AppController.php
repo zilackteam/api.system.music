cdc4cd5280352565f651034b56c28ad07387f93a
Tien Nguyen <tiennv@atmarkcafe.org>
2017-04-27 14:55:07 +0700

    Fix API

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -5,16 +5,25 @@ namespace App\Http\Controllers;
 use App\Http\Requests;
 use App\Models\App;
 use App\Models\Authentication;
-use Request;
 use Auth;
+use Illuminate\Http\Request;
 
 class AppController extends Controller {
+
     public function index(Request $request) {
         //
         try {
-            $apps = App::query();
+            $auth = Authentication::findOrFail($request->auth_id);
 
-            $apps = $apps->get();
+            if ($auth->level == Authentication::AUTH_ADMIN) {
+                $apps = App::query();
+
+                $apps = $apps->get();
+            } else {
+                $apps = App::join('app_managers', 'app_managers.app_id', '=', 'apps.id')
+                    ->where('app_managers.manager_id', $auth->id)
+                    ->get();
+            }
 
             return $this->responseSuccess($apps);
         } catch (\Exception $e) {
